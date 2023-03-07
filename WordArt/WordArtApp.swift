7fada2db60bf39ab430c5e97b22c7913ae5f7847
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2023-03-07 07:47:38 -0500

    Initial Commit

---
+++ WordArt/WordArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordArtApp.swift
+//  WordArt
+//
+//  Created by Joseph Adam Iannazzone on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
