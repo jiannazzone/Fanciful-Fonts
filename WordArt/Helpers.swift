4b58712f102cdf235541138fd82856b1b86d7e4b
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2023-03-08 10:37:23 -0500

    - Separate convertText() from ContentView - Add description parameter to struct FancyText and add init method

--- WordArt/Helpers.swift
+++ WordArt/Helpers.swift
@@ -36,5 +36,199 @@ extension String {
 
 struct FancyText: Identifiable {
     let id = UUID()
-    var value = ""
+    let description: String
+    var value = String()
+    
+    init (_ desc: String) {
+        description = desc
+    }
 }
+
+func convertText(userInput: String) -> [FancyText] {
+    
+    var outputs = [FancyText]()
+    
+    let sanitizedInput = userInput.applyingTransform(.stripDiacritics, reverse: false) ?? ""
+    
+    var stringAsUnicode = [Int]()
+    for char in sanitizedInput.unicodeScalars {
+        stringAsUnicode.append(Int(char.value))
+    }
+    
+    // Full-Width Romaji
+    var fullWidth = FancyText("Full Width")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        if thisChar == " " {
+            fullWidth.value += "　"
+        } else if thisChar == "." {
+            fullWidth.value += "．"
+        } else {
+            fullWidth.value += String(UnicodeScalar(num + 65248) ?? UnicodeScalar(0))
+        }
+    }
+    outputs.append(fullWidth)
+    
+    // Circle Text
+    var circleText = FancyText("Circles")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isNumber {
+            circleText.value += String(UnicodeScalar(num + 9263) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            circleText.value += String(UnicodeScalar(num + 9327) ?? UnicodeScalar(0))
+        } else if thisChar.isUppercase {
+            circleText.value += String(UnicodeScalar(num + 9333) ?? UnicodeScalar(0))
+        } else {
+            circleText.value += String(thisChar)
+        }
+    }
+    outputs.append(circleText)
+    
+    // Sharp Box Text
+    var sharpBox = FancyText("Boxes")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isLowercase {
+            sharpBox.value += String(UnicodeScalar(num + 127183) ?? UnicodeScalar(0))
+        } else if thisChar.isUppercase {
+            sharpBox.value += String(UnicodeScalar(num + 127215) ?? UnicodeScalar(0))
+        } else {
+            sharpBox.value += String(thisChar)
+        }
+    }
+    outputs.append(sharpBox)
+    
+    // Strikethrough Text
+    var strikethroughText = FancyText("Strikethrough")
+    for char in userInput {
+        strikethroughText.value += String(char)
+        strikethroughText.value += String(UnicodeScalar(822) ?? UnicodeScalar(0))
+    }
+    outputs.append(strikethroughText)
+    
+    // Underline Text
+    var underlineText = FancyText("Underline")
+    for char in userInput {
+        underlineText.value += String(char)
+        underlineText.value += String(UnicodeScalar(817) ?? UnicodeScalar(0))
+    }
+    outputs.append(underlineText)
+    
+    // Sponge Text
+    var spongeText = FancyText("Sarcastic")
+    for i in 0..<userInput.count {
+        if i % 2 == 0 {
+            spongeText.value += userInput[i].lowercased()
+        } else {
+            spongeText.value += userInput[i].uppercased()
+        }
+    }
+    outputs.append(spongeText)
+    
+    // Bold Text Serif
+    var boldTextSerif = FancyText("Bold Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isUppercase {
+            boldTextSerif.value += String(UnicodeScalar(num + 119743) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            boldTextSerif.value += String(UnicodeScalar(num + 119737) ?? UnicodeScalar(0))
+        } else {
+            boldTextSerif.value += String(thisChar)
+        }
+    }
+    outputs.append(boldTextSerif)
+    
+    // Bold Text Sans
+    var boldTextSans = FancyText("Bold Sans Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isUppercase {
+            boldTextSans.value += String(UnicodeScalar(num + 120211) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            boldTextSans.value += String(UnicodeScalar(num + 120205) ?? UnicodeScalar(0))
+        } else {
+            boldTextSans.value += String(thisChar)
+        }
+    }
+    outputs.append(boldTextSans)
+    
+    // Italic Text Serif
+    var italicTextSerif = FancyText("Italic Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar == "h" {
+            italicTextSerif.value += "ℎ"
+        } else if thisChar.isUppercase {
+            italicTextSerif.value += String(UnicodeScalar(num + 119795) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            italicTextSerif.value += String(UnicodeScalar(num + 119789) ?? UnicodeScalar(0))
+        } else {
+            italicTextSerif.value += String(thisChar)
+        }
+    }
+    outputs.append(italicTextSerif)
+    
+    // Italic Text Sans
+    var italicTextSans = FancyText("Italic Sans Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isUppercase {
+            italicTextSans.value += String(UnicodeScalar(num + 120263) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            italicTextSans.value += String(UnicodeScalar(num + 120257) ?? UnicodeScalar(0))
+        } else {
+            italicTextSans.value += String(thisChar)
+        }
+    }
+    outputs.append(italicTextSans)
+    
+    // Bold Italic Serif
+    var boldItalicSerif = FancyText("Bold Italic Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isUppercase {
+            boldItalicSerif.value += String(UnicodeScalar(num + 119847) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            boldItalicSerif.value += String(UnicodeScalar(num + 119841) ?? UnicodeScalar(0))
+        } else {
+            boldItalicSerif.value += String(thisChar)
+        }
+    }
+    outputs.append(boldItalicSerif)
+    
+    // Bold Italic Sans
+    var boldItalicSans = FancyText("Bold Italic Sans Serif")
+    for i in 0..<stringAsUnicode.count {
+        let num = stringAsUnicode[i]
+        let thisChar = Character(UnicodeScalar(num) ?? UnicodeScalar(0))
+        
+        if thisChar.isUppercase {
+            boldItalicSans.value += String(UnicodeScalar(num + 120315) ?? UnicodeScalar(0))
+        } else if thisChar.isLowercase {
+            boldItalicSans.value += String(UnicodeScalar(num + 120309) ?? UnicodeScalar(0))
+        } else {
+            boldItalicSans.value += String(thisChar)
+        }
+    }
+    outputs.append(boldItalicSans)
+    
+    return outputs
+    
+} // convertText
